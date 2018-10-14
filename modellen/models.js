3d007ded73ad818828294d646dc6758aa78dee98
Tom Kooij <tomkooij@tomkooij.nl>
2018-10-14 21:30:45 +0200

    Add auto en schaatser

--- modellen/models.js
+++ modellen/models.js
@@ -4,7 +4,9 @@ var model_index = [
     {url:'modellen/constantesnelheid.xml', title: 'Constante snelheidsmodel'},
     {url:'modellen/constanteversnelling.xml', title: 'Constante versnellingsmodel'},
     {url:'modellen/krachtversnelling.xml', title: 'Kracht-versnellingsmodel'},
-    {url:'modellen/opgave_schaatser.xml', title: 'Opgave 10 schaatser'},
+    {url:'modellen/opgave_auto.xml', title: 'Opgave 18 auto (antwoord)'},
+    {url:'modellen/opgave_schaatser.xml', title: 'Opgave 20 schaatser (antwoord)'},
+    {url:'modellen/opgave_fietser.xml', title: 'Opgave 22 skydiver (antwoord)'},
     {url:'modellen/opgave_fietser.xml', title: 'Opgave 28 fietser (antwoord)'},
     {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave 31 voetbal verticaal'},
 ];
