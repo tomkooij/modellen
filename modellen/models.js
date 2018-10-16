4e189ee33da7edee91b3db6e3700e7ae41400ab2
Tom Kooij <tomkooij@tomkooij.nl>
2018-10-16 14:28:52 +0200

    Fix url of skydiver model

--- modellen/models.js
+++ modellen/models.js
@@ -6,7 +6,7 @@ var model_index = [
     {url:'modellen/krachtversnelling.xml', title: 'Kracht-versnellingsmodel'},
     {url:'modellen/opgave_auto.xml', title: 'Opgave 18 auto (antwoord)'},
     {url:'modellen/opgave_schaatser.xml', title: 'Opgave 20 schaatser (antwoord)'},
-    {url:'modellen/opgave_fietser.xml', title: 'Opgave 22 skydiver (antwoord)'},
+    {url:'modellen/opgave_skydiver.xml', title: 'Opgave 22 skydiver (antwoord)'},
     {url:'modellen/opgave_fietser.xml', title: 'Opgave 28 fietser (antwoord)'},
     {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave 31 voetbal verticaal'},
 ];
