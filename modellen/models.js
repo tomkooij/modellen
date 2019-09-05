38e96375f11ca2fbe651c17aec3c39420d9c8737
Tom Kooij <tomkooij@tomkooij.nl>
2019-09-05 10:41:27 +0200

    Aanpassingen voor module klas 4 C2 Energie
    H3 uit titel
    variable s -> x (ds -> dx)
    leeg model toegevoegd
    X-stream examen 2017 toegevoegd

--- modellen/models.js
+++ modellen/models.js
@@ -1,12 +1,14 @@
 // definieer hier de modellen
 
 var model_index = [
+    {url:'modellen/leeg.xml', title: 'Lege modelomgeving'},
     {url:'modellen/constantesnelheid.xml', title: 'Constante snelheidsmodel'},
     {url:'modellen/constanteversnelling.xml', title: 'Constante versnellingsmodel'},
     {url:'modellen/krachtversnelling.xml', title: 'Kracht-versnellingsmodel'},
-    {url:'modellen/opgave_auto.xml', title: 'Opgave 18 auto (antwoord)'},
-    {url:'modellen/opgave_schaatser.xml', title: 'Opgave 20 schaatser (antwoord)'},
-    {url:'modellen/opgave_skydiver.xml', title: 'Opgave 22 skydiver (antwoord)'},
-    {url:'modellen/opgave_fietser.xml', title: 'Opgave 28 fietser (antwoord)'},
-    {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave 31 voetbal verticaal'},
+    {url:'modellen/opgave_auto.xml', title: 'Opgave auto (antwoord)'},
+    {url:'modellen/opgave_schaatser.xml', title: 'Opgave schaatser (antwoord)'},
+    {url:'modellen/opgave_skydiver.xml', title: 'Opgave skydiver (antwoord)'},
+    {url:'modellen/opgave_fietser.xml', title: 'Opgave fietser (antwoord)'},
+    {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave voetbal verticaal'},
+    {url:'modellen/opgave_x-stream.xml', title: 'Opgave X-Stream'},
 ];
