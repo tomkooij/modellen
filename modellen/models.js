7c7e44ef940030fc089f7e8788eda16ffaf8fb86
Tom Kooij <tomkooij@tomkooij.nl>
2019-09-16 15:10:32 +0200

    Add antwoord van optrekken met constante kracht

--- modellen/models.js
+++ modellen/models.js
@@ -13,6 +13,7 @@ var model_index = [
     {url:'modellen/opgave_voetbalverticaal.xml', title: 'Opgave voetbal verticaal'},
     {url:'modellen/opgave_x-stream.xml', title: 'Opgave X-Stream'},
     {url:'modellen/opgave_optrekkenmetconstantekracht.xml', title: 'Opgave optrekken met constante kracht'},
+    {url:'modellen/opgave_optrekkenmetconstantekracht_antwoord.xml', title: 'Opgave optrekken met constante kracht (antwoord)'},
     {url:'modellen/opgave_optrekkenmetconstantvermogen.xml', title: 'Opgave optrekken met constant vermogen'},
     {url:'modellen/opgave_uitrijdenvaneenauto.xml', title: 'Opgave uitrijden van een auto'},
     {url:'modellen/opgave_buckeyebullet.xml', title: 'Opgave Buckeye bullet'},
