723455fdee4bd5f1f1d9817d3927bbbf05285ce9
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-01 10:37:46 +0100

    Update model.js to new models

--- modellen/models.js
+++ modellen/models.js
@@ -19,4 +19,21 @@ var model_index = [
     {url:'modellen/opgave_uitrijdenvaneenauto.xml', title: 'Opgave uitrijden van een auto'},
     {url:'modellen/opgave_buckeyebullet.xml', title: 'Opgave Buckeye bullet'},
 
+    {url:'modellen/Val_zonder_luchtweerstand_(fysische_vrije_val).xml', title: 'Val zonder luchtweerstand (fysische vrije val)'},
+    {url:'modellen/Val_zonder_luchtweerstand_(fysische_vrije_val)_UITWERKING.xml', title: 'Val zonder luchtweerstand (fysische vrije val) UITWERKING'},
+    {url:'modellen/Val_met_luchtweerstand.xml', title: 'Val met luchtweerstand'},
+    {url:'modellen/Val_met_luchtweerstand_UITWERKING.xml', title: 'Val met luchtweerstand UITWERKING'},
+
+    {url:'modellen/Horizontaal_massa_veersysteem.xml', title: 'Horizontaal massa veersysteem'},
+
+    {url:'modellen/Arbeid_op_een_verticaal_geschoten_voetbal.xml', title: 'Arbeid op een verticaal geschoten voetbal'},
+    {url:'modellen/Arbeid_op_een_verticaal_geschoten_voetbal_UITWERKING1.xml', title: 'Arbeid op een verticaal geschoten voetbal UITWERKING1'},
+    {url:'modellen/Arbeid_op_een_verticaal_geschoten_voetbal_UITWERKING2.xml', title: 'Arbeid op een verticaal geschoten voetbal UITWERKING2'},
+    {url:'modellen/Bungeejumper.xml', title: 'Bungeejumper'},
+    {url:'modellen/Bungeejumper_UITWERKING.xml', title: 'Bungeejumper UITWERKING'},
+    {url:'modellen/Bungeejumper_UITWERKING2.xml', title: 'Bungeejumper UITWERKING2'},
+    {url:'modellen/Een_sprong_bij_volleybal_(examen_vwo_2015_I).xml', title: 'Een sprong bij volleybal (examen vwo 2015 I)'},
+    {url:'modellen/Een_sprong_bij_volleybal_(examen_vwo_2015_I)_UITWERKING.xml', title: 'Een sprong bij volleybal (examen vwo 2015 I) UITWERKING'},
+
+
 ];
